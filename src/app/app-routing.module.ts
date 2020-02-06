ce33b71e526e7d6e67a9174d1ff7e3d4145dce2d
Erlantzcg89 <erlantzcg89@gmail.com>
2020-02-06 10:44:16 +0100

    recetario component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecetarioComponent } from './paginas/recetario/recetario.component';
 
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: RecetarioComponent },
+  { path: '**', redirectTo: "/inicio" }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    appRoutes,
+    { enableTracing: true }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
